73b6ff17de994f458d8ca58c25a83f9856adb4eb
Joyjoyreyes <mary_021257@binalatongan.edu.ph>
2024-06-09 20:09:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,34 @@ function clickLike2(){
 }  
 btnLike2.addEventListener("click",clickLike2)
 
+const btnDisLike1 = document.getElementById("btnDisLike1")
+const countDisLike1 = document.getElementById("countDisLike1")
+
+function clickDisLike1(){
+  let totalDisLikes = parseInt(countDisLike1.value) + 1
+  countDisLike1.textContent = totalDisLikes.toString()
+}
+btnDisLike1.addEventListener("click",clickDisLike1)
+
+const btnDisLike2 = document.getElementById("btnDisLike2")
+const countDisLikes2 = document.getElementById("countDisLike2")
+
+function clickDisLike2(){
+  let totalDisLikes = parseInt(countDisLike2.value) + 1
+  countDisLike2.textContent = totalDisLikes.toString()
+}
+btnDisLike2.addEventListener("click", clickDisLike2)
+
 const comment = document.getElementById("comment")
+
 const comment = document.getElementById("submit")
+
 const comment = document.getElementById("commentbox")
 
 function submitComment(){
-     commentbox.textContent += comment.value.toString() + "\n
-     comment.value=""
+
+commentbox.textContent += comment.value.toString() + "\n
+comment.value=""
 }
+
 submit.addEventListener("click", submitComment)
